7b7ed96887faa451b4819ede3913ad4449162496
pogoks <kbjun97@naver.com>
2020-07-29 22:50:40 +0900

    2020/07/29 DB팀 회원정보 불러오기 구현
    1. 로그인 시 회원 정보 불러오기 기능 추가.
    2. 로그인 되어있을 경우 메뉴 눌렀을 때 회원 이름이랑 회원 이메일 뜨고,
       로그아웃 누르면 다시 비로그인 계정이라고 나옵니다!
    3. 메인 화면 예약하기, 메뉴 화면 예약하기와 예약확인
       객실안내 화면 예약하기 기능에 로그인 여부 체크하는거 넣었습니다.
    
    [파일 추가 : 회원 정보 불러오기 위한 UserInfo 클래스 추가]

--- app/src/main/java/com/cookandroid/luna_hotel/Login_gloval.java
+++ app/src/main/java/com/cookandroid/luna_hotel/Login_gloval.java
@@ -4,8 +4,8 @@ import android.app.Application;
 
 public class Login_gloval extends Application {
 
-    // 전연변수 쓰는 클래스입니다
-    public static String login_id;  // 아이디 전연변수입니다.
+    // 전역변수 쓰는 클래스입니다
+    public static String login_id;  // 아이디 전역변수 입니다.
     public static String login_password; // 비밀번호 전역변수 입니다.
 
     @Override
