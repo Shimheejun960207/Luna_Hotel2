76e826e14256ea23624fa20e5033d98980dc94b3
shimheejun <shimmyungsic@naver.com>
2020-08-03 14:21:21 +0900

    + 시설안내 지점안내 UI 기본틀 생성

---
+++ app/src/main/java/com/cookandroid/luna_hotel/Luna_Establishment_Info.java
@@ -0,0 +1,6 @@
+package com.cookandroid.luna_hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Luna_Establishment_Info extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/cookandroid/luna_hotel/Luna_Local_Hotel_Info.java
@@ -0,0 +1,8 @@
+package com.cookandroid.luna_hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Luna_Local_Hotel_Info extends AppCompatActivity {
+
+
+}
