be04f57387fc0c5da7849585b9551799dabaefd6
shimheejun <shimmyungsic@naver.com>
2020-08-05 14:14:07 +0900

    + 프로모션 UI 생성 광고1번 UI 생성

--- app/src/main/java/com/cookandroid/luna_hotel/Luna_Copy1.java
+++ app/src/main/java/com/cookandroid/luna_hotel/Luna_Copy1.java
@@ -9,57 +9,21 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Luna_Promotion extends AppCompatActivity {
+public class Luna_Copy1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.luna_promotion);
+        setContentView(R.layout.luna_copy1);
 
 
         Button btn_back,btn_lunalogo;
         ImageView btn_copy1,btn_copy2,btn_copy3,btn_copy4;
 
-        btn_copy1 = (ImageView) findViewById(R.id.btn_copy1);
-        btn_copy2 = (ImageView) findViewById(R.id.btn_copy2);
-        btn_copy3 = (ImageView) findViewById(R.id.btn_copy3);
-        btn_copy4 = (ImageView) findViewById(R.id.btn_copy4);
+
         btn_back = (Button)findViewById(R.id.btn_back);
         btn_lunalogo = (Button)findViewById(R.id.btn_lunalogo);
 
-        // 1번 광고클릭
-        btn_copy1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent Home_Intent = new Intent(getApplicationContext(), Luna_Copy1.class);
-                startActivity(Home_Intent);
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
-            }
-        });
-
-        // 2번 광고클릭
-        btn_copy2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        // 3번 광고클릭
-        btn_copy3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        // 4번 광고클릭
-        btn_copy4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
         //뒤로버튼 클릭 메소드
         btn_back.setOnClickListener(new View.OnClickListener() {
